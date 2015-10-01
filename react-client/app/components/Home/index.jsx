04b811c67d4421e24a354bd8cbead2ec21a91d98
uhjish <ajish@rootedinsights.com>
2015-10-01 18:50:55 -0400

    adding two way bindings for text grid and grid text

--- react-client/app/components/Home/index.jsx
+++ react-client/app/components/Home/index.jsx
@@ -4,27 +4,68 @@ var React        = require('react'),
     mui          = require('material-ui'),
     RaisedButton = mui.RaisedButton,
     Snackbar     = mui.Snackbar,
+    TextField    = mui.TextField,
     Matrix       = require('../Matrix/matrix.jsx');
 
 module.exports = React.createClass({
+
+  mixins: [React.addons.LinkedStateMixin],
+
+  getInitialState: function() {
+    return {
+      word:"eac",
+      rows:[["a","d"],["b","e"],["e","f"]]
+    };
+  }, 
+  
   render: function() {
-    var matrixCols = [['a','b','c'],['d','e','f']]; 
+    console.log(this.state);
+    //TODO: inefficient, move elsewhere
+    var transpose = function( rs ){
+      return rs[0].map(function (_, c) { return rs.map(function (r) { return r[c]; }); });
+    };
+    var toRowsString = function( rs ){
+      return rs.map( function( r ){ return r.join(","); } ).join(";");
+    };
+
     return <div className='homePage pageContent'>
       <h1>Wordsearch Grid</h1>
       <p>Scalatra, React, Webpack and Material UI.</p>
       <Matrix
           ref='matrix' 
-          columns={matrixCols}
+          columns={transpose(this.state.rows)}
           resize='horizontal' />
       <br/>
-      <RaisedButton label='Update' primary={true} onTouchTap={this._handleClick} />
       <br/>
+      <TextField
+          ref='gridtext'
+          hintText="enter grid rows, cells separated by commas, rows separate by semicolons (ex: a,b,c;d,e,f;g,h,I)"
+          floatingLabelText="Input Grid Rows" 
+          value={toRowsString(this.state.rows)}
+          onChange={this.handleGridTextChange} />
+      <br/>
+      <TextField
+          ref='searchword'
+          hintText="word to search for in grid"
+          floatingLabelText="Search Word" 
+          valueLink={this.linkState('word')}/>
+      <br/>
+      <br/>
+      <RaisedButton label='Search' primary={true} onTouchTap={this._handleClick} />
       <Snackbar
         ref='snackbar'
         message={'Updating...'} />
     </div>;
   },
-
+  handleGridTextChange: function(event) {
+    var newgt = this.refs.gridtext.getValue();
+    console.log(newgt);
+    this.setState(
+      {rows: newgt.split(";")
+                  .map(function(x){ return x.split(",")}),
+       word: this.refs.searchword.value});
+    console.log(this.state);
+  },
   _handleClick: function() {
     this.refs.snackbar.show();
     this.refs.matrix.toggleCells([{'x':0,'y':0},{'x':1,'y':1},{'x':0,'y':2}]);
