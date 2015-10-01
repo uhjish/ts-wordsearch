1edb75ed6095b393f7141d96a71760ef894f4c16
uhjish <ajish@rootedinsights.com>
2015-09-30 20:57:11 -0400

    adding backtrack dfs logic

--- wordsearch.sh
+++ wordsearch.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 exec scala "$0" "$@"
 !#
-object HelloWorld {
+object WordSearch {
+
+def dfsBacktrack(path: List[(Int,Int)], curX: Int, curY: Int, cells: List[List[String]], srch: String) = {
+    val updPath = (curX, curY) :: path
+    val depth = updPath.length
+
+    for (xoffs <- -1 to 1; yoffs <- -1 to 1){
+      dfsBacktrack( updPath, curX+xoffs, curY+yoffs, cells, srch)
+    }
+  }
+
   def main(args: Array[String]) {
-    val nrow = args(0).toInt
-    val ncol = args(1).toInt
-    val cells = args(2).split(",").map(x => x.trim.toInt).grouped(ncol).map(_.toList).toList
+    val word = args(0).trim.toLowerCase
+    val nrow = args(1).toInt
+    val ncol = args(2).toInt
+    val cells = args(3).split(",").map(x => x.trim.toLowerCase).grouped(ncol).map(_.toList).toList
     println("Hello, world! " + cells)
   }
 }
-HelloWorld.main(args)
+WordSearch.main(args)
