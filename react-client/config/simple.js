d7a1ffe256d21aa97e2b429d27b4811a5c4df92a
uhjish <ajish@rootedinsights.com>
2015-10-01 23:53:06 -0400

    removing react client from webservice branch

--- react-client/config/simple.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var fs   = require('fs'),
-    path = require('path'),
-    html = fs.readFileSync(path.resolve(__dirname, '../app/simple.html'), 'utf-8');
-
-module.exports = function(path, scriptUrl, styleUrl, commonsUrl, callback) {
-  callback(null, html.replace('SCRIPT_URL', scriptUrl));
-};
--- react-client/lib/server-production.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-require('./server')({
-  prerender: true,
-  separateStylesheet: true,
-  defaultPort: 8081
-});
